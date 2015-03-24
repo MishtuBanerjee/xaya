99477b733762d87cc589c1b901f99d8d47711188
Mishtu Banerjee <mishtu.banerjee@tesera.com>
2015-03-24 01:14:54 -0600

    xaya_tutorial.py setup scripts_dir 20150323 mb

---
+++ scripts/xaya_tutorial.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+"""
+xaya_tutorial: Putting the various xaya modules through theirpaes ...
+
+BeginDate:20040902
+CurrentRevisionDate:20060202
+Development Version : core 001
+Release Version: pre-release
+
+Author(s): Mishtu Banerjee, Tyler Mitchell
+Contact: mishtu_banerjee@shaw.ca
+
+Copyright: 2004-2015, The Authors
+License: Distributed under MIT License
+    [http://opensource.org/licenses/mit-license.html]
+    
+Original Environment: Programmed and tested under Python 2.7.X
+ 
+Dependencies:
+    Python Interpreter and base libraries.
+    xaya: xayacore, xaystats, xayadb, xayanet, xayabee
+
+"""
+import xayacore
