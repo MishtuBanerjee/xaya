4818b02292383df122b19c5f59a8e4dd6da0dbb8
Mishtu Banerjee <mishtu.banerjee@tesera.com>
2015-03-24 03:13:22 -0600

    __init__.py setup xaya 20150324 mb

---
+++ xaya/__init__.py
@@ -0,0 +1,34 @@
+"""
+xaya
+=====
+
+Author(s): Mishtu Banerjee
+Contact: mishtu_banerjee@shaw.ca
+
+Copyright: 2004-2015, The Authors
+License: Distributed under MIT License
+Consult LICENSE.txt or http://opensource.org/licenses/mit-license.html
+
+Using
+-----
+
+Creating a Graph
+Adding Edges
+Adding Nodes
+
+
+"""
+
+
+
+from __future__ import absolute_import
+
+# xayacore has core graph functions
+from xaya.xayacore import *
+
+# xayastasts has core statistical functions
+from xaya.xayastats import *
+
+
+
+__version__ = '0.1.10' 
